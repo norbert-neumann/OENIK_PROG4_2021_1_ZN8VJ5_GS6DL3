79b70f76b4ae4a3296da7c35dd0611c18e7234cc
neumann_norbert <neumann.norbert.norbert@gmail.com>
2021-03-26 17:46:04 +0100

    Added abstract game object

---
+++ OENIK_PROG4_2021_1_ZN8VJ5_GS6DL3/Warcraft.GameModel/GameObject.cs
@@ -0,0 +1,50 @@
+﻿namespace Warcraft
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Drawing;
+    using System.Linq;
+    using System.Numerics;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows;
+
+    /// <summary>
+    /// Abstract game object class: each object that is rendered has to have a position nad
+    /// the we have to change the positon somehow. This class implements this functionality.
+    /// </summary>
+    public abstract class GameObject
+    {
+        /// <summary>
+        /// X coordinate of the object.
+        /// </summary>
+        public int X { get; set; }
+
+        /// <summary>
+        /// Y coordinate of the object.
+        /// </summary>
+        public int Y { get; set; }
+
+        /// <summary>
+        /// Sets the object's position to a new one.
+        /// </summary>
+        /// <param name="newX">New X coordinate.</param>
+        /// <param name="newY">New Y coordinate.</param>
+        public void SetPosition(int newX, int newY)
+        {
+            this.X = newX;
+            this.Y = newY;
+        }
+
+        /// <summary>
+        /// Changes the object's position by a given amount.
+        /// </summary>
+        /// <param name="dx">Change in x coordinate.</param>
+        /// <param name="dy">Change in y coordinate.</param>
+        public void ChangePosition(int dx, int dy)
+        {
+            this.X += dx;
+            this.Y += dy;
+        }
+    }
+}
