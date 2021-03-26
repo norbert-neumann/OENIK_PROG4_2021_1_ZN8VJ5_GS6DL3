6095bbb87f1fa3120d5f6f355203f4cd541f6840
neumann_norbert <neumann.norbert.norbert@gmail.com>
2021-03-26 17:43:31 +0100

    Added IStorageRepository

---
+++ OENIK_PROG4_2021_1_ZN8VJ5_GS6DL3/Warcraft.Repository/IStorageRepo.cs
@@ -0,0 +1,35 @@
+﻿namespace Warcraft
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Stores all saved games and highscore.
+    /// Can save games, and reload them.
+    /// </summary>
+    public interface IStorageRepo
+    {
+        /// <summary>
+        /// Saves a game state to a file.
+        /// </summary>
+        /// <param name="game">Game state to save.</param>
+        /// <param name="name">File name to save to.</param>
+        void Save(IGameModel game, string name);
+
+        /// <summary>
+        /// Loads a given game state.
+        /// </summary>
+        /// <param name="name">File name where the game is saved.</param>
+        /// <returns>The reloaded game state.</returns>
+        IGameModel Load(string name);
+
+        /// <summary>
+        /// Gives the high scores in a descending order.
+        /// </summary>
+        /// <returns>High scores :).</returns>
+        List<KeyValuePair<string, TimeSpan>> GetHighScores();
+    }
+}
