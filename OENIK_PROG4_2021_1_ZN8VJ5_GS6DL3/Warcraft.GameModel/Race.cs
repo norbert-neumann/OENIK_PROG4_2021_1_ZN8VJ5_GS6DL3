717bd1b638431f51b10e4e77d9a8a03d49d4dcee
neumann_norbert <neumann.norbert.norbert@gmail.com>
2021-03-26 17:48:31 +0100

    Added enum that lists all available races

---
+++ OENIK_PROG4_2021_1_ZN8VJ5_GS6DL3/Warcraft.GameModel/Race.cs
@@ -0,0 +1,24 @@
+﻿namespace Warcraft
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Race :).
+    /// </summary>
+    public enum Race
+    {
+        /// <summary>
+        /// Human race.
+        /// </summary>
+        Human,
+
+        /// <summary>
+        /// Orc race.
+        /// </summary>
+        Orc,
+    }
+}
