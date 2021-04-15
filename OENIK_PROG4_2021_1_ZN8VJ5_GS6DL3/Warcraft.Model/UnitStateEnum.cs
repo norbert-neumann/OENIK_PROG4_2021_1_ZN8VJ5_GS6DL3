c56f32472b45d2c00afc22faecf7d2c382868e20
neumann_norbert <neumann.norbert.norbert@gmail.com>
2021-04-15 21:17:30 +0200

    Added enum that describes a unit's state. These states can be assoiciated with a sprite

---
+++ OENIK_PROG4_2021_1_ZN8VJ5_GS6DL3/Warcraft.Model/UnitStateEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Warcraft.Model
+{
+    public enum UnitStateEnum
+    {
+        Walking,
+        WalkingWithLumber,
+        WalkingWithGold,
+        Fighting
+    }
+}
