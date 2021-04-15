ed658ce8329cb25665739806b6c5ed3875a9d719
neumann_norbert <neumann.norbert.norbert@gmail.com>
2021-04-15 21:19:30 +0200

    Added building enum

---
+++ OENIK_PROG4_2021_1_ZN8VJ5_GS6DL3/Warcraft.Model/BuildingEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Warcraft.Model
+{
+    public enum BuildingEnum
+    {
+        Hall,
+        Farm,
+        Barracks
+    }
+}
