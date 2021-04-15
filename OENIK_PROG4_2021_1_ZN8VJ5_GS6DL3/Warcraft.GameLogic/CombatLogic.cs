da3b20f334317e7007d9c4845694b09f9ed785e8
neumann_norbert <neumann.norbert.norbert@gmail.com>
2021-04-15 21:24:35 +0200

    Added combat logic, whose job is to detect two close enemy units and set their target's to eachother

---
+++ OENIK_PROG4_2021_1_ZN8VJ5_GS6DL3/Warcraft.GameLogic/CombatLogic.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using Warcraft.Model;
+
+namespace Warcraft.GameLogic
+{
+    public class CombatLogic
+    {
+        public GameModel model;
+
+        public CombatLogic(GameModel model)
+        {
+            this.model = model;
+        }
+
+        // Check if agressive object reached it's enemy. If not set target. If yes flip UnitStateEnum
+        public void SetTarget()
+        {
+            foreach (Unit unit in model.playerUnits)
+            {
+                if (unit.enemy != null)
+                {
+                    if ((unit.enemy as Building) != null)
+                    {
+                        if (unit.IsPositionInHitbox(unit.enemy))
+                        {
+                            unit.UnitState = UnitStateEnum.Fighting;
+                        }
+                        else
+                        {
+                            unit.UnitState = UnitStateEnum.Walking;
+                            unit.target = unit.enemy.Position;
+                        }
+                    }
+                    else
+                    {
+                        if (unit.Distance(unit.enemy) <= unit.range)
+                        {
+                            unit.UnitState = UnitStateEnum.Fighting;
+                        }
+                        else
+                        {
+                            unit.UnitState = UnitStateEnum.Walking;
+                            unit.target = unit.enemy.Position;
+                        }
+                    }
+                }
+                else
+                {
+                    Unit enemy = FindEnemyInRange(unit, model.enemyUnits);
+                    if (enemy != null)
+                    {
+                        // We set the target both ways so no need to search for enemies in the next loop
+                        unit.enemy = enemy;
+                        enemy.enemy = unit;
+                    }
+                }
+            }
+
+            foreach (Unit unit in model.enemyUnits)
+            {
+                if (unit.enemy != null)
+                {
+                    if ((unit.enemy as Building) != null)
+                    {
+                        if (unit.IsPositionInHitbox(unit.enemy))
+                        {
+                            unit.UnitState = UnitStateEnum.Fighting;
+                        }
+                        else
+                        {
+                            unit.UnitState = UnitStateEnum.Walking;
+                            unit.target = unit.enemy.Position;
+                        }
+                    }
+                    else
+                    {
+                        if (unit.Distance(unit.enemy) <= unit.range)
+                        {
+                            unit.UnitState = UnitStateEnum.Fighting;
+                        }
+                        else
+                        {
+                            unit.UnitState = UnitStateEnum.Walking;
+                            unit.target = unit.enemy.Position;
+                        }
+                    }
+                }
+            }
+        }
+
+        private Unit FindEnemyInRange(Unit self, List<Unit> enemies)
+        {
+            foreach (Unit enemy in enemies)
+            {
+                if (self.Distance(enemy) <= Config.AggroRange)
+                {
+                    return enemy;
+                }
+            }
+            return null;
+        }
+    }
+}
