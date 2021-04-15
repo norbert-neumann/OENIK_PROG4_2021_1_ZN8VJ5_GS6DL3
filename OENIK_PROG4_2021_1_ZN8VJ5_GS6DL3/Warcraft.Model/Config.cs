b7ffdaee5826317dacaac4781f1854031add792a
neumann_norbert <neumann.norbert.norbert@gmail.com>
2021-04-15 21:23:04 +0200

    Added config file, containing all base variables

---
+++ OENIK_PROG4_2021_1_ZN8VJ5_GS6DL3/Warcraft.Model/Config.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Warcraft.Model
+{
+    public static class Config
+    {
+        public static Point HumanTownHallPosition = new Point(5, 5);
+        public static Point OrcTownHallPosition = new Point(5, 5);
+        public static int GoldUnit = 21;
+        public static int LumberUnit = 17;
+        public static int Speed = 3;
+        public static int BorderWidth = 10;
+        public static double DefaultThreshold = 0.8;
+        public static double AggroRange = 50;
+        public static double DistanceThreshold = 5;
+
+        public static string AsString(UnitStateEnum state)
+        {
+            switch (state)
+            {
+                case UnitStateEnum.Walking:
+                    return "W";
+                case UnitStateEnum.WalkingWithLumber:
+                    return "WT";
+                case UnitStateEnum.WalkingWithGold:
+                    return "WG";
+                case UnitStateEnum.Fighting:
+                    return "F";
+                default:
+                    return "";
+            }
+        }
+
+        // Move this to logic
+        public static int GetAnimationLength(UnitStateEnum state)
+        {
+            if (state == UnitStateEnum.WalkingWithGold)
+            {
+                return 4;
+            }
+            return 5;
+        } 
+    }
+}
