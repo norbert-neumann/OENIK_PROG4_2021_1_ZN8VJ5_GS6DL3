efeb259ce8b4a292410c1b0a404b8981fda57b9c
neumann_norbert <neumann.norbert.norbert@gmail.com>
2021-04-15 21:27:56 +0200

    Implemented core logic

---
+++ OENIK_PROG4_2021_1_ZN8VJ5_GS6DL3/Warcraft.GameLogic/CoreLogic.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Warcraft.Model;
+
+namespace Warcraft.GameLogic
+{
+    public class CoreLogic
+    {
+        private GameModel model;
+        private CombatLogic combatLogic;
+        private MovementLogic movementLogic;
+        private AnimationLogic animationLogic;
+        private PathfindingLogic pathfindingLogic;
+
+        public CoreLogic(GameModel model, CombatLogic combatLogic, MovementLogic movementLogic, AnimationLogic animationLogic, PathfindingLogic pathfindingLogic)
+        {
+            this.model = model;
+            this.combatLogic = combatLogic;
+            this.movementLogic = movementLogic;
+            this.animationLogic = animationLogic;
+            this.pathfindingLogic = pathfindingLogic;
+        }
+
+        // Run before rendering
+        public void Step()
+        {
+            combatLogic.SetTarget();
+            movementLogic.UpdatePositions();
+            animationLogic.UpdateSprites();
+        }
+    }
+}
