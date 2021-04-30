59642acb25f621cf3d4538868481aa94f49b70fe
neumann_norbert <neumann.norbert.norbert@gmail.com>
2021-04-30 08:46:17 +0200

    Added timers to controller

--- OENIK_PROG4_2021_1_ZN8VJ5_GS6DL3/Warcraft.GameControl/TestController.cs
+++ OENIK_PROG4_2021_1_ZN8VJ5_GS6DL3/Warcraft.GameControl/TestController.cs
@@ -1,15 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
-using System.Windows.Media;
-using Warcraft.GameLogic;
-using Warcraft.Model;
-using Warcraft.Renderer;
-using System.Drawing;
-
-namespace Warcraft.GameControl
+﻿namespace Warcraft.GameControl
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Drawing;
+    using System.Text;
+    using System.Windows;
+    using System.Windows.Media;
+    using System.Windows.Threading;
+    using Warcraft.GameLogic;
+    using Warcraft.Model;
+    using Warcraft.Renderer;
+
+    /// <summary>
+    /// Game controller.
+    /// </summary>
     public class TestController : FrameworkElement
     {
         GameModel model;
@@ -20,74 +25,90 @@ namespace Warcraft.GameControl
         AnimationLogic animationLogic;
         TestRenderer renderer;
 
+        private DispatcherTimer timer;
+        private DispatcherTimer animationTimer;
+
         public TestController()
         {
-            Loaded += TestLoading;
+            this.Loaded += this.TestLoading;
         }
 
         private void TestLoading(object sender, RoutedEventArgs e)
         {
-            model = new GameModel(SystemParameters.PrimaryScreenWidth, SystemParameters.PrimaryScreenHeight);
+            this.model = new GameModel(SystemParameters.PrimaryScreenWidth, SystemParameters.PrimaryScreenHeight);
+            BuildingFactory factory = new BuildingFactory(this.model);
 
-            pathfindingLogic = new PathfindingLogic();
-            combatLogic = new CombatLogic(model);
-            movementLogic = new MovementLogic(model, pathfindingLogic);
-            animationLogic = new AnimationLogic(model);
-            logic = new CoreLogic(model, combatLogic, movementLogic, animationLogic, pathfindingLogic);
+            this.pathfindingLogic = new PathfindingLogic();
+            this.combatLogic = new CombatLogic(this.model);
+            this.movementLogic = new MovementLogic(this.model, this.pathfindingLogic);
+            this.animationLogic = new AnimationLogic(this.model);
+            this.logic = new CoreLogic(this.model, this.combatLogic, this.movementLogic, this.animationLogic, this.pathfindingLogic);
 
-            renderer = new TestRenderer(model);
+            this.renderer = new TestRenderer(model);
 
-            
-            Building orcHall = new Building(BuildingEnum.Hall, RaceEnum.Orc, 100, 100, 120, 120);
-            Building orcFarm = new Building(BuildingEnum.Farm, RaceEnum.Orc, 100, 290, 90, 80);
-            Building orcBarrack = new Building(BuildingEnum.Barracks, RaceEnum.Orc, 300, 150, 120, 120);
 
-            Building humanHall = new Building(BuildingEnum.Hall, RaceEnum.Human, 1500, 600, 120, 120);
-            Building humanFarm = new Building(BuildingEnum.Farm, RaceEnum.Human, 1500, 800, 90, 80);
-            Building humanBarrack = new Building(BuildingEnum.Barracks, RaceEnum.Human, 1700, 600, 120, 120);
-
-            Unit human = new Unit(RaceEnum.Human, UnitTypeEnum.Peasant, 1700, 810, 30, 30);
-            Unit orc = new Unit(RaceEnum.Orc, UnitTypeEnum.Peasant, 300, 350, 30, 30);
-
+            Unit human = new Unit(OwnerEnum.PLAYER, RaceEnum.Human, UnitTypeEnum.Peasant, 600, 600, (int)(30 * Config.Zoom), (int)(30 * Config.Zoom));
             human.path = new Queue<System.Drawing.Point>();
-            orc.path = new Queue<System.Drawing.Point>();
+            human.target = new System.Drawing.Point(10, 10);
 
-            human.target = new System.Drawing.Point(450, 450);
-            orc.target = new System.Drawing.Point(450, 450);
+            GoldMine mine = new GoldMine(150, 150, 140, 140, 500);
 
-            model.playerUnits.Add(human);
-            model.enemyUnits.Add(orc);
+            Building hall = factory.Create("player human hall", 600, 400);
+            model.goldMines.Add(mine);
 
-            model.playerBuildings.Add(humanFarm);
-            model.playerBuildings.Add(humanBarrack);
-            model.playerBuildings.Add(humanHall);
+            movementLogic.routines.Add(human, new GoldMiningRoutine(human, TimeSpan.FromSeconds(2), hall, mine));
+            //logic.routines.Add(human, new GoldMiningRoutine(human, TimeSpan.FromSeconds(2), new System.Drawing.Point(100, 100), new System.Drawing.Point(300, 300)));
 
-            model.enemyBuildings.Add(orcFarm);
-            model.enemyBuildings.Add(orcHall);
-            model.enemyBuildings.Add(orcBarrack);
 
+
+            model.units.Add(human);
 
             Window win = Window.GetWindow(this);
             if (win != null)
             {
-                win.KeyDown += Win_KeyDown;
+                win.KeyDown += this.Win_KeyDown;
+                this.timer = new DispatcherTimer();
+                this.timer.Interval = TimeSpan.FromMilliseconds(45);
+                this.timer.Tick += this.TimerTick;
+                this.timer.Start();
+
+                this.animationTimer = new DispatcherTimer();
+                this.animationTimer.Interval = TimeSpan.FromMilliseconds(90);
+                this.animationTimer.Tick += this.AnimationTimer_Tick;
+                this.animationTimer.Start();
             }
 
-            InvalidateVisual();
+            this.InvalidateVisual();
+        }
+
+        /// <inheritdoc/>
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            if (this.renderer != null)
+            {
+                drawingContext.DrawDrawing(renderer.BuildDrawing());
+            }
+        }
+
+        private void AnimationTimer_Tick(object sender, EventArgs e)
+        {
+            this.logic.UpdateAnimation();
+            this.InvalidateVisual();
+        }
+
+        private void TimerTick(object sender, EventArgs e)
+        {
+            this.logic.Step();
+            this.InvalidateVisual();
         }
 
         private void Win_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
         {
             if (e.Key == System.Windows.Input.Key.Enter)
             {
-                logic.Step();
-                InvalidateVisual();
+                this.logic.Step();
+                this.InvalidateVisual();
             }
-        }
-
-        protected override void OnRender(DrawingContext drawingContext)
-        {
-            if (renderer != null) drawingContext.DrawDrawing(renderer.BuildDrawing());
         }
     }
 }
