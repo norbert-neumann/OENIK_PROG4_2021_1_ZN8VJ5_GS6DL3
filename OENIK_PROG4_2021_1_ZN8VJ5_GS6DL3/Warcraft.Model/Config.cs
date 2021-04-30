a89e823a60fb409bc8f314c534a94980912504b5
neumann_norbert <neumann.norbert.norbert@gmail.com>
2021-04-30 08:33:44 +0200

    Made stylecop happy

--- OENIK_PROG4_2021_1_ZN8VJ5_GS6DL3/Warcraft.Model/Config.cs
+++ OENIK_PROG4_2021_1_ZN8VJ5_GS6DL3/Warcraft.Model/Config.cs
@@ -1,10 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Text;
-
-namespace Warcraft.Model
+﻿namespace Warcraft.Model
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Drawing;
+    using System.Text;
+
+    /// <summary>
+    /// Static class containg configurable constants.
+    /// Most fileds will be moved to a specific logic class so I wont document these yet.
+    /// </summary>
     public static class Config
     {
         public static Point HumanTownHallPosition = new Point(5, 5);
