d1af694821d860da46a9c099b0cf52db35429530
neumann_norbert <neumann.norbert.norbert@gmail.com>
2021-04-30 08:44:39 +0200

    Encapsulated every logic in this class. controller will call this

--- OENIK_PROG4_2021_1_ZN8VJ5_GS6DL3/Warcraft.GameLogic/CoreLogic.cs
+++ OENIK_PROG4_2021_1_ZN8VJ5_GS6DL3/Warcraft.GameLogic/CoreLogic.cs
@@ -1,10 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Warcraft.Model;
-
-namespace Warcraft.GameLogic
+﻿namespace Warcraft.GameLogic
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using Warcraft.Model;
+
+    /// <summary>
+    /// Encapsulates and calls each task specific logic.
+    /// The Controller also calls this logic's methods.
+    /// </summary>
     public class CoreLogic
     {
         private GameModel model;
@@ -13,6 +17,14 @@ namespace Warcraft.GameLogic
         private AnimationLogic animationLogic;
         private PathfindingLogic pathfindingLogic;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CoreLogic"/> class.
+        /// </summary>
+        /// <param name="model">Game model to operate on.</param>
+        /// <param name="combatLogic">Combat logic instance.</param>
+        /// <param name="movementLogic">Movement logic instance.</param>
+        /// <param name="animationLogic">Animatin logic instance.</param>
+        /// <param name="pathfindingLogic">Pathfinding logic instance.</param>
         public CoreLogic(GameModel model, CombatLogic combatLogic, MovementLogic movementLogic, AnimationLogic animationLogic, PathfindingLogic pathfindingLogic)
         {
             this.model = model;
@@ -23,11 +35,51 @@ namespace Warcraft.GameLogic
         }
 
         // Run before rendering
+
+        /// <summary>
+        /// One step of the game.
+        /// </summary>
         public void Step()
         {
-            combatLogic.SetTarget();
-            movementLogic.UpdatePositions();
-            animationLogic.UpdateSprites();
+            this.combatLogic.SetTarget();
+            this.movementLogic.UpdatePositions();
+            this.Remove();
+        }
+
+        /// <summary>
+        /// Updates each unit's sprite.
+        /// </summary>
+        public void UpdateAnimation()
+        {
+            this.animationLogic.UpdateSprites();
+        }
+
+        private void Remove()
+        {
+            foreach (Unit unit in this.model.unitsToRemove)
+            {
+                this.model.units.Remove(unit);
+            }
+
+            foreach (Building b in this.model.buildingsToRemove)
+            {
+                this.model.buildings.Remove(b);
+            }
+
+            foreach (CombatObject t in this.model.treesToRemove)
+            {
+                this.model.lumberMines.Remove(t);
+            }
+
+            foreach (GoldMine m in this.model.goldMinesToRemove)
+            {
+                this.model.goldMines.Remove(m);
+            }
+
+            this.model.unitsToRemove.Clear();
+            this.model.buildingsToRemove.Clear();
+            this.model.treesToRemove.Clear();
+            this.model.goldMinesToRemove.Clear();
         }
     }
 }
