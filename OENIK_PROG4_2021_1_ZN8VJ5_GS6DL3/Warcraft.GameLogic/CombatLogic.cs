47cd5f8f7a84f09b8825159f88746d4ccbf937c2
neumann_norbert <neumann.norbert.norbert@gmail.com>
2021-04-30 08:40:54 +0200

    Moved combat logic to it's own class

--- OENIK_PROG4_2021_1_ZN8VJ5_GS6DL3/Warcraft.GameLogic/CombatLogic.cs
+++ OENIK_PROG4_2021_1_ZN8VJ5_GS6DL3/Warcraft.GameLogic/CombatLogic.cs
@@ -1,24 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Text;
-using Warcraft.Model;
-
-namespace Warcraft.GameLogic
+﻿namespace Warcraft.GameLogic
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Drawing;
+    using System.Text;
+    using Warcraft.Model;
+
+    /// <summary>
+    /// Deals with everything realted to combat.
+    /// </summary>
     public class CombatLogic
     {
+        /// <summary>
+        /// Game model to operate on.
+        /// </summary>
         public GameModel model;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CombatLogic"/> class.
+        /// </summary>
+        /// <param name="model">Game model to operate on.</param>
         public CombatLogic(GameModel model)
         {
             this.model = model;
         }
 
-        // Check if agressive object reached it's enemy. If not set target. If yes flip UnitStateEnum
+        /// <summary>
+        /// Check if agressive object reached it's enemy. If not set target. If yes flip UnitStateEnum.
+        /// </summary>
         public void SetTarget()
         {
-            foreach (Unit unit in model.playerUnits)
+            foreach (Unit unit in this.model.units)
             {
                 if (unit.enemy != null)
                 {
@@ -26,7 +38,7 @@ namespace Warcraft.GameLogic
                     {
                         if (unit.IsPositionInHitbox(unit.enemy))
                         {
-                            unit.UnitState = UnitStateEnum.Fighting;
+                            this.AttackEnemyBuilding(unit);
                         }
                         else
                         {
@@ -38,7 +50,7 @@ namespace Warcraft.GameLogic
                     {
                         if (unit.Distance(unit.enemy) <= unit.range)
                         {
-                            unit.UnitState = UnitStateEnum.Fighting;
+                            this.AttackEnemyUnit(unit);
                         }
                         else
                         {
@@ -49,57 +61,70 @@ namespace Warcraft.GameLogic
                 }
                 else
                 {
-                    Unit enemy = FindEnemyInRange(unit, model.enemyUnits);
+                    CombatObject enemy = this.FindEnemyInRange(unit);
                     if (enemy != null)
                     {
-                        // We set the target both ways so no need to search for enemies in the next loop
                         unit.enemy = enemy;
-                        enemy.enemy = unit;
-                    }
-                }
-            }
-
-            foreach (Unit unit in model.enemyUnits)
-            {
-                if (unit.enemy != null)
-                {
-                    if ((unit.enemy as Building) != null)
-                    {
-                        if (unit.IsPositionInHitbox(unit.enemy))
-                        {
-                            unit.UnitState = UnitStateEnum.Fighting;
-                        }
-                        else
-                        {
-                            unit.UnitState = UnitStateEnum.Walking;
-                            unit.target = unit.enemy.Position;
-                        }
-                    }
-                    else
-                    {
-                        if (unit.Distance(unit.enemy) <= unit.range)
-                        {
-                            unit.UnitState = UnitStateEnum.Fighting;
-                        }
-                        else
-                        {
-                            unit.UnitState = UnitStateEnum.Walking;
-                            unit.target = unit.enemy.Position;
-                        }
+                        unit.target = enemy.Position;
+                        unit.inIdle = false;
                     }
                 }
             }
         }
 
-        private Unit FindEnemyInRange(Unit self, List<Unit> enemies)
+        private void AttackEnemyUnit(Unit unit)
         {
-            foreach (Unit enemy in enemies)
+            unit.UnitState = UnitStateEnum.Fighting;
+            if (unit.enemy.AcceptDamage(unit.attack))
             {
-                if (self.Distance(enemy) <= Config.AggroRange)
+                this.model.unitsToRemove.Add(unit.enemy as Unit);
+                this.ResetUnit(unit);
+            }
+        }
+
+        private void AttackEnemyBuilding(Unit unit)
+        {
+            unit.UnitState = UnitStateEnum.Fighting;
+            if (unit.enemy.AcceptDamage(unit.attack))
+            {
+                this.model.buildingsToRemove.Add(unit.enemy as Building);
+                this.ResetUnit(unit);
+            }
+        }
+
+        private void ResetUnit(Unit unit)
+        {
+            unit.enemy = null;
+            unit.UnitState = UnitStateEnum.Walking;
+        }
+
+        private void HarvestLumber(Unit unit)
+        {
+            unit.UnitState = UnitStateEnum.Fighting;
+            if (unit.enemy.AcceptDamage(unit.attack))
+            {
+                this.model.treesToRemove.Add(unit.enemy as CombatObject);
+            }
+        }
+
+        private CombatObject FindEnemyInRange(Unit self)
+        {
+            foreach (Unit other in this.model.units)
+            {
+                if (self.Owner != other.Owner && self.Distance(other) <= Config.AggroRange)
                 {
-                    return enemy;
+                    return other;
                 }
             }
+
+            foreach (Building buildng in this.model.buildings)
+            {
+                if (self.Owner != buildng.Owner && self.Distance(buildng) <= Config.AggroRange)
+                {
+                    return buildng;
+                }
+            }
+
             return null;
         }
     }
