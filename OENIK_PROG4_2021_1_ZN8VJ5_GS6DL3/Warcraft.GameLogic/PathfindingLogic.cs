9bd3b758753c743071ca9871aaefb4c462f0feb0
neumann_norbert <neumann.norbert.norbert@gmail.com>
2021-04-30 10:08:29 +0200

    Changed local variables to start with lower case letters

--- OENIK_PROG4_2021_1_ZN8VJ5_GS6DL3/Warcraft.GameLogic/PathfindingLogic.cs
+++ OENIK_PROG4_2021_1_ZN8VJ5_GS6DL3/Warcraft.GameLogic/PathfindingLogic.cs
@@ -19,15 +19,15 @@
         /// <param name="trajectory">The units trajectory.</param>
         public void FindPath(Unit unit, GameObject collison, Point trajectory)
         {
-            Point UpperLeftBorder = new Point(collison.hitbox.X - Config.BorderWidth, collison.hitbox.Y - Config.BorderWidth);
-            Point UpperRightBorder = new Point(collison.hitbox.Right + Config.BorderWidth, collison.hitbox.Y - Config.BorderWidth);
-            Point BottomLeftBorder = new Point(collison.hitbox.X - Config.BorderWidth, collison.hitbox.Bottom + Config.BorderWidth);
-            Point BottomRightBorder = new Point(collison.hitbox.Right + Config.BorderWidth, collison.hitbox.Bottom + Config.BorderWidth);
-            Point startPoint = unit.prevPosition;
-            Point endPoint = new Point(trajectory.X * collison.hitbox.Width, trajectory.Y * collison.hitbox.Height);
+            Point upperLeftBorder = new Point(collison.Hitbox.X - Config.BorderWidth, collison.Hitbox.Y - Config.BorderWidth);
+            Point upperRightBorder = new Point(collison.Hitbox.Right + Config.BorderWidth, collison.Hitbox.Y - Config.BorderWidth);
+            Point bottomLeftBorder = new Point(collison.Hitbox.X - Config.BorderWidth, collison.Hitbox.Bottom + Config.BorderWidth);
+            Point bottomRightBorder = new Point(collison.Hitbox.Right + Config.BorderWidth, collison.Hitbox.Bottom + Config.BorderWidth);
+            Point startPoint = unit.PrevPosition;
+            Point endPoint = new Point(trajectory.X * collison.Hitbox.Width, trajectory.Y * collison.Hitbox.Height);
             endPoint.X += startPoint.X + 20;
             endPoint.Y += startPoint.Y + 20;
-            endPoint = this.CalculateEndPoint(unit.target, collison);
+            endPoint = this.CalculateEndPoint(unit.Target, collison);
 
             Queue<Point> pathA = new Queue<Point>();
             Queue<Point> pathB = new Queue<Point>();
@@ -38,144 +38,144 @@
             // Now calculate the two possible paths
 
             // LEFT
-            if (startPoint.X <= collison.hitbox.X - 1)
+            if (startPoint.X <= collison.Hitbox.X - 1)
             {
                 // Path A: Up + Right + (Down)
                 // Up
-                pathALength += this.L1Distance(startPoint, UpperLeftBorder);
-                pathA.Enqueue(UpperLeftBorder);
-                if (UpperLeftBorder.Y != endPoint.Y)
+                pathALength += this.L1Distance(startPoint, upperLeftBorder);
+                pathA.Enqueue(upperLeftBorder);
+                if (upperLeftBorder.Y != endPoint.Y)
                 {
-                    pathALength += this.L1Distance(UpperLeftBorder, UpperRightBorder);
-                    pathA.Enqueue(UpperRightBorder);
-                    pathALength += this.L1Distance(UpperRightBorder, endPoint);
+                    pathALength += this.L1Distance(upperLeftBorder, upperRightBorder);
+                    pathA.Enqueue(upperRightBorder);
+                    pathALength += this.L1Distance(upperRightBorder, endPoint);
                 }
                 else
                 {
-                    pathALength += this.L1Distance(UpperLeftBorder, endPoint);
+                    pathALength += this.L1Distance(upperLeftBorder, endPoint);
                 }
 
                 pathA.Enqueue(endPoint);
 
                 // Path B: Down + Right + (Up)
-                pathB.Enqueue(BottomLeftBorder);
-                if (BottomLeftBorder.X != endPoint.Y)
+                pathB.Enqueue(bottomLeftBorder);
+                if (bottomLeftBorder.X != endPoint.Y)
                 {
-                    pathBLength += this.L1Distance(BottomLeftBorder, BottomRightBorder);
-                    pathB.Enqueue(BottomRightBorder);
-                    pathBLength += this.L1Distance(BottomRightBorder, endPoint);
+                    pathBLength += this.L1Distance(bottomLeftBorder, bottomRightBorder);
+                    pathB.Enqueue(bottomRightBorder);
+                    pathBLength += this.L1Distance(bottomRightBorder, endPoint);
                 }
                 else
                 {
-                    pathBLength += this.L1Distance(BottomLeftBorder, endPoint);
+                    pathBLength += this.L1Distance(bottomLeftBorder, endPoint);
                 }
 
                 pathB.Enqueue(endPoint);
             }
 
             // RIGHT
-            else if (startPoint.X >= collison.hitbox.Right + 1)
+            else if (startPoint.X >= collison.Hitbox.Right + 1)
             {
                 // PATH A: Up + Left + Down
-                pathA.Enqueue(UpperRightBorder);
-                pathALength += this.L1Distance(startPoint, UpperRightBorder);
-                if (UpperRightBorder.X != endPoint.X)
+                pathA.Enqueue(upperRightBorder);
+                pathALength += this.L1Distance(startPoint, upperRightBorder);
+                if (upperRightBorder.X != endPoint.X)
                 {
-                    pathALength += this.L1Distance(UpperRightBorder, UpperLeftBorder);
-                    pathA.Enqueue(UpperLeftBorder);
-                    pathALength += this.L1Distance(UpperLeftBorder, endPoint);
+                    pathALength += this.L1Distance(upperRightBorder, upperLeftBorder);
+                    pathA.Enqueue(upperLeftBorder);
+                    pathALength += this.L1Distance(upperLeftBorder, endPoint);
                 }
                 else
                 {
-                    pathALength += this.L1Distance(UpperRightBorder, endPoint);
+                    pathALength += this.L1Distance(upperRightBorder, endPoint);
                 }
 
                 pathA.Enqueue(endPoint);
 
                 // PATH B: Down + Left + Up
-                pathB.Enqueue(BottomRightBorder);
-                pathBLength += this.L1Distance(startPoint, BottomRightBorder);
-                if (BottomRightBorder.X != endPoint.X)
+                pathB.Enqueue(bottomRightBorder);
+                pathBLength += this.L1Distance(startPoint, bottomRightBorder);
+                if (bottomRightBorder.X != endPoint.X)
                 {
-                    pathBLength += this.L1Distance(BottomRightBorder, BottomLeftBorder);
-                    pathB.Enqueue(BottomLeftBorder);
-                    pathBLength += this.L1Distance(BottomLeftBorder, endPoint);
+                    pathBLength += this.L1Distance(bottomRightBorder, bottomLeftBorder);
+                    pathB.Enqueue(bottomLeftBorder);
+                    pathBLength += this.L1Distance(bottomLeftBorder, endPoint);
                 }
                 else
                 {
-                    pathBLength += this.L1Distance(BottomRightBorder, endPoint);
+                    pathBLength += this.L1Distance(bottomRightBorder, endPoint);
                 }
 
                 pathB.Enqueue(endPoint);
             }
 
             // UP
-            else if (startPoint.Y <= collison.hitbox.Y - 1)
+            else if (startPoint.Y <= collison.Hitbox.Y - 1)
             {
                 // PATH A: Right + Down + Left
-                pathA.Enqueue(UpperRightBorder);
-                pathALength += this.L1Distance(startPoint, UpperRightBorder);
-                if (UpperRightBorder.X != endPoint.X)
+                pathA.Enqueue(upperRightBorder);
+                pathALength += this.L1Distance(startPoint, upperRightBorder);
+                if (upperRightBorder.X != endPoint.X)
                 {
-                    pathALength += this.L1Distance(UpperRightBorder, BottomRightBorder);
-                    pathA.Enqueue(BottomRightBorder);
-                    pathALength += this.L1Distance(BottomRightBorder, endPoint);
+                    pathALength += this.L1Distance(upperRightBorder, bottomRightBorder);
+                    pathA.Enqueue(bottomRightBorder);
+                    pathALength += this.L1Distance(bottomRightBorder, endPoint);
                 }
                 else
                 {
-                    pathALength += this.L1Distance(UpperRightBorder, endPoint);
+                    pathALength += this.L1Distance(upperRightBorder, endPoint);
                 }
 
                 pathA.Enqueue(endPoint);
 
                 // PATH B: Left + Down + Right
-                pathB.Enqueue(UpperLeftBorder);
-                pathBLength += this.L1Distance(startPoint, UpperLeftBorder);
-                if (UpperLeftBorder.X != endPoint.X)
+                pathB.Enqueue(upperLeftBorder);
+                pathBLength += this.L1Distance(startPoint, upperLeftBorder);
+                if (upperLeftBorder.X != endPoint.X)
                 {
-                    pathBLength += this.L1Distance(BottomLeftBorder, UpperLeftBorder);
-                    pathB.Enqueue(BottomLeftBorder);
-                    pathBLength += this.L1Distance(BottomLeftBorder, endPoint);
+                    pathBLength += this.L1Distance(bottomLeftBorder, upperLeftBorder);
+                    pathB.Enqueue(bottomLeftBorder);
+                    pathBLength += this.L1Distance(bottomLeftBorder, endPoint);
                 }
                 else
                 {
-                    pathBLength += this.L1Distance(UpperLeftBorder, endPoint);
+                    pathBLength += this.L1Distance(upperLeftBorder, endPoint);
                 }
 
                 pathB.Enqueue(endPoint);
             }
 
             // DOWN
-            else if (startPoint.Y >= collison.hitbox.Bottom + 1)
+            else if (startPoint.Y >= collison.Hitbox.Bottom + 1)
             {
                 // PATH A: Right + Up + Left
-                pathA.Enqueue(BottomRightBorder);
-                pathALength += this.L1Distance(startPoint, BottomRightBorder);
-                if (BottomRightBorder.X != endPoint.X)
+                pathA.Enqueue(bottomRightBorder);
+                pathALength += this.L1Distance(startPoint, bottomRightBorder);
+                if (bottomRightBorder.X != endPoint.X)
                 {
-                    pathALength += this.L1Distance(UpperRightBorder, BottomRightBorder);
-                    pathA.Enqueue(UpperRightBorder);
-                    pathALength += this.L1Distance(UpperRightBorder, endPoint);
+                    pathALength += this.L1Distance(upperRightBorder, bottomRightBorder);
+                    pathA.Enqueue(upperRightBorder);
+                    pathALength += this.L1Distance(upperRightBorder, endPoint);
                 }
                 else
                 {
-                    pathALength += this.L1Distance(BottomRightBorder, endPoint);
+                    pathALength += this.L1Distance(bottomRightBorder, endPoint);
                 }
 
                 pathA.Enqueue(endPoint);
 
                 // PATH B: Left + Up + Right
-                pathB.Enqueue(BottomLeftBorder);
-                pathBLength += this.L1Distance(startPoint, BottomLeftBorder);
-                if (BottomLeftBorder.X != endPoint.X)
+                pathB.Enqueue(bottomLeftBorder);
+                pathBLength += this.L1Distance(startPoint, bottomLeftBorder);
+                if (bottomLeftBorder.X != endPoint.X)
                 {
-                    pathBLength += this.L1Distance(BottomLeftBorder, UpperLeftBorder);
-                    pathB.Enqueue(UpperLeftBorder);
-                    pathBLength += this.L1Distance(UpperLeftBorder, endPoint);
+                    pathBLength += this.L1Distance(bottomLeftBorder, upperLeftBorder);
+                    pathB.Enqueue(upperLeftBorder);
+                    pathBLength += this.L1Distance(upperLeftBorder, endPoint);
                 }
                 else
                 {
-                    pathBLength += this.L1Distance(BottomLeftBorder, endPoint);
+                    pathBLength += this.L1Distance(bottomLeftBorder, endPoint);
                 }
 
                 pathB.Enqueue(endPoint);
@@ -183,84 +183,84 @@
 
             if (pathALength < pathBLength)
             {
-                unit.path = pathA;
+                unit.Path = pathA;
             }
             else
             {
-                unit.path = pathB;
+                unit.Path = pathB;
             }
         }
 
         private Point CalculateEndPoint(Point target, GameObject collison)
         {
             Point endPoint = new Point(target.X, target.Y);
-            Point UpperLeftBorder = new Point(collison.hitbox.X - Config.BorderWidth, collison.hitbox.Y - Config.BorderWidth);
-            Point UpperRightBorder = new Point(collison.hitbox.Right + Config.BorderWidth, collison.hitbox.Y - Config.BorderWidth);
-            Point BottomLeftBorder = new Point(collison.hitbox.X - Config.BorderWidth, collison.hitbox.Bottom + Config.BorderWidth);
-            Point BottomRightBorder = new Point(collison.hitbox.Right + Config.BorderWidth, collison.hitbox.Bottom + Config.BorderWidth);
+            Point upperLeftBorder = new Point(collison.Hitbox.X - Config.BorderWidth, collison.Hitbox.Y - Config.BorderWidth);
+            Point upperRightBorder = new Point(collison.Hitbox.Right + Config.BorderWidth, collison.Hitbox.Y - Config.BorderWidth);
+            Point bottomLeftBorder = new Point(collison.Hitbox.X - Config.BorderWidth, collison.Hitbox.Bottom + Config.BorderWidth);
+            Point bottomRightBorder = new Point(collison.Hitbox.Right + Config.BorderWidth, collison.Hitbox.Bottom + Config.BorderWidth);
 
-            double UpperLeftDistance = this.L1Distance(UpperLeftBorder, target);
-            double UpperRightDistance = this.L1Distance(UpperRightBorder, target);
-            double BottomLeftDistance = this.L1Distance(BottomLeftBorder, target);
-            double BottomRightDistance = this.L1Distance(BottomRightBorder, target);
+            double upperLeftDistance = this.L1Distance(upperLeftBorder, target);
+            double upperRightDistance = this.L1Distance(upperRightBorder, target);
+            double bottomLeftDistance = this.L1Distance(bottomLeftBorder, target);
+            double bottomRightDistance = this.L1Distance(bottomRightBorder, target);
 
-            double min = Math.Min(Math.Min(UpperLeftDistance, UpperRightDistance), Math.Min(BottomLeftDistance, BottomRightDistance));
+            double min = Math.Min(Math.Min(upperLeftDistance, upperRightDistance), Math.Min(bottomLeftDistance, bottomRightDistance));
 
-            if (min == UpperLeftDistance)
+            if (min == upperLeftDistance)
             {
-                if (Math.Abs(endPoint.X - UpperLeftBorder.X) >= Math.Abs(endPoint.Y - UpperLeftBorder.Y))
+                if (Math.Abs(endPoint.X - upperLeftBorder.X) >= Math.Abs(endPoint.Y - upperLeftBorder.Y))
                 {
-                    endPoint.X = collison.hitbox.Left - Config.BorderWidth;
+                    endPoint.X = collison.Hitbox.Left - Config.BorderWidth;
                 }
 
-                if (Math.Abs(endPoint.Y - UpperLeftBorder.Y) >= Math.Abs(endPoint.X - UpperLeftBorder.X))
+                if (Math.Abs(endPoint.Y - upperLeftBorder.Y) >= Math.Abs(endPoint.X - upperLeftBorder.X))
                 {
-                    endPoint.Y = collison.hitbox.Top - Config.BorderWidth;
+                    endPoint.Y = collison.Hitbox.Top - Config.BorderWidth;
                 }
             }
-            else if (min == UpperRightDistance)
+            else if (min == upperRightDistance)
             {
-                if (Math.Abs(endPoint.X - UpperLeftBorder.X) >= Math.Abs(endPoint.Y - UpperLeftBorder.Y))
+                if (Math.Abs(endPoint.X - upperLeftBorder.X) >= Math.Abs(endPoint.Y - upperLeftBorder.Y))
                 {
-                    endPoint.X = collison.hitbox.Right + Config.BorderWidth;
+                    endPoint.X = collison.Hitbox.Right + Config.BorderWidth;
                 }
 
-                if (Math.Abs(endPoint.Y - UpperLeftBorder.Y) >= Math.Abs(endPoint.X - UpperLeftBorder.X))
+                if (Math.Abs(endPoint.Y - upperLeftBorder.Y) >= Math.Abs(endPoint.X - upperLeftBorder.X))
                 {
-                    endPoint.Y = collison.hitbox.Top - Config.BorderWidth;
+                    endPoint.Y = collison.Hitbox.Top - Config.BorderWidth;
                 }
             }
-            else if (min == BottomLeftDistance)
+            else if (min == bottomLeftDistance)
             {
-                if (Math.Abs(endPoint.X - UpperLeftBorder.X) >= Math.Abs(endPoint.Y - UpperLeftBorder.Y))
+                if (Math.Abs(endPoint.X - upperLeftBorder.X) >= Math.Abs(endPoint.Y - upperLeftBorder.Y))
                 {
-                    endPoint.X = collison.hitbox.Left - Config.BorderWidth;
+                    endPoint.X = collison.Hitbox.Left - Config.BorderWidth;
                 }
 
-                if (Math.Abs(endPoint.Y - UpperLeftBorder.Y) >= Math.Abs(endPoint.X - UpperLeftBorder.X))
+                if (Math.Abs(endPoint.Y - upperLeftBorder.Y) >= Math.Abs(endPoint.X - upperLeftBorder.X))
                 {
-                    endPoint.Y = collison.hitbox.Bottom + Config.BorderWidth;
+                    endPoint.Y = collison.Hitbox.Bottom + Config.BorderWidth;
                 }
             }
-            else if (min == BottomRightDistance)
+            else if (min == bottomRightDistance)
             {
-                if (Math.Abs(endPoint.X - UpperLeftBorder.X) >= Math.Abs(endPoint.Y - UpperLeftBorder.Y))
+                if (Math.Abs(endPoint.X - upperLeftBorder.X) >= Math.Abs(endPoint.Y - upperLeftBorder.Y))
                 {
-                    endPoint.X = collison.hitbox.Right + Config.BorderWidth;
+                    endPoint.X = collison.Hitbox.Right + Config.BorderWidth;
                 }
 
-                if (Math.Abs(endPoint.Y - UpperLeftBorder.Y) >= Math.Abs(endPoint.X - UpperLeftBorder.X))
+                if (Math.Abs(endPoint.Y - upperLeftBorder.Y) >= Math.Abs(endPoint.X - upperLeftBorder.X))
                 {
-                    endPoint.Y = collison.hitbox.Bottom + Config.BorderWidth;
+                    endPoint.Y = collison.Hitbox.Bottom + Config.BorderWidth;
                 }
             }
 
             return endPoint;
         }
 
-        private double L1Distance(Point A, Point B)
+        private double L1Distance(Point a, Point b)
         {
-            return Math.Abs(A.X - B.X) + Math.Abs(A.Y - B.Y);
+            return Math.Abs(a.X - b.X) + Math.Abs(a.Y - b.Y);
         }
     }
 }
