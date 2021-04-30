f3774f0475bfef2241e7bdd912fa606817a2785b
neumann_norbert <neumann.norbert.norbert@gmail.com>
2021-04-30 08:48:16 +0200

    Added factory that creates a mine (goldmine/tree) described by the parameters

---
+++ OENIK_PROG4_2021_1_ZN8VJ5_GS6DL3/Warcraft.GameLogic/MineFactory.cs
@@ -0,0 +1,68 @@
+﻿namespace Warcraft.GameLogic
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Warcraft.Model;
+
+    /// <summary>
+    /// Creates a mine based on a description.
+    /// </summary>
+    public class MineFactory
+    {
+        private GameModel model;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MineFactory"/> class.
+        /// </summary>
+        /// <param name="model">Game model to operaete on.</param>
+        public MineFactory(GameModel model)
+        {
+            this.model = model;
+        }
+
+        /// <summary>
+        /// Creates a mine based on a description.
+        /// </summary>
+        /// <param name="description">What mine to create.</param>
+        /// <param name="x">X pos.</param>
+        /// <param name="y">Y pos.</param>
+        /// <returns>The newly created game object.</returns>
+        public GameObject Create(string description, int x, int y)
+        {
+            string[] components = description.ToUpper().Split(' ');
+
+            int width = 0;
+            int height = 0;
+
+            if (components[0] == "GOLD")
+            {
+                width = 130;
+                height = 120;
+
+                width = (int)(width * Config.Zoom);
+                height = (int)(height * Config.Zoom);
+
+                GoldMine goldMine = new GoldMine(x, y, width, height, int.Parse(components[1]));
+                this.model.goldMines.Add(goldMine);
+
+                return goldMine;
+            }
+            else
+            {
+                width = 110;
+                height = 100;
+
+                width = (int)(width * Config.Zoom);
+                height = (int)(height * Config.Zoom);
+
+                CombatObject tree = new CombatObject(x, y, width, height);
+                this.model.lumberMines.Add(tree);
+
+                return tree;
+            }
+        }
+    }
+}
