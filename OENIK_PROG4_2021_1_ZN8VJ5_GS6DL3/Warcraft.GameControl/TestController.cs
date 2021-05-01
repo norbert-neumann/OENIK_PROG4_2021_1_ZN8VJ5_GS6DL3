b4c68354be7655e878dcda0240b126e6327e717f
neumann_norbert <neumann.norbert.norbert@gmail.com>
2021-05-01 18:42:00 +0200

    Changed KeyDown event for testing

--- OENIK_PROG4_2021_1_ZN8VJ5_GS6DL3/Warcraft.GameControl/TestController.cs
+++ OENIK_PROG4_2021_1_ZN8VJ5_GS6DL3/Warcraft.GameControl/TestController.cs
@@ -112,10 +112,9 @@
 
         private void Win_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
         {
-            if (e.Key == System.Windows.Input.Key.Enter)
+            if (e.Key == System.Windows.Input.Key.G)
             {
-                this.logic.Step();
-                this.InvalidateVisual();
+                this.logic.MineGold();
             }
         }
     }
