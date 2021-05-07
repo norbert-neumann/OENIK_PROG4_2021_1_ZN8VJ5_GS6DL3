bcaa94a6d4b50fe88150ee24e98cbe8c08d12ca2
neumann_norbert <neumann.norbert.norbert@gmail.com>
2021-05-07 20:46:21 +0200

    Changed return values

--- OENIK_PROG4_2021_1_ZN8VJ5_GS6DL3/Warcraft.Repository/IStorageRepo.cs
+++ OENIK_PROG4_2021_1_ZN8VJ5_GS6DL3/Warcraft.Repository/IStorageRepo.cs
@@ -5,6 +5,7 @@
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
+    using Warcraft.Model;
 
     /// <summary>
     /// Stores all saved games and highscore.
@@ -15,16 +16,16 @@
         /// <summary>
         /// Saves a game state to a file.
         /// </summary>
-        /// <param name="game">Game state to save.</param>
+        /// <param name="model">Game state to save.</param>
         /// <param name="name">File name to save to.</param>
-        void Save(string name);
+        void Save(GameModel model, string name);
 
         /// <summary>
         /// Loads a given game state.
         /// </summary>
         /// <param name="name">File name where the game is saved.</param>
         /// <returns>The reloaded game state.</returns>
-        //IGameModel Load(string name);
+        GameModel Load(string name);
 
         /// <summary>
         /// Gives the high scores in a descending order.
