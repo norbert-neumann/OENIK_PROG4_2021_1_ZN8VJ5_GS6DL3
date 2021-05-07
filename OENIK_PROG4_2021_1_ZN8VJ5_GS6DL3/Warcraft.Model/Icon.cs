7ab1b33456927793e515a07cfb79d2edd6b1b880
neumann_norbert <neumann.norbert.norbert@gmail.com>
2021-05-07 20:48:13 +0200

    Added icon class

---
+++ OENIK_PROG4_2021_1_ZN8VJ5_GS6DL3/Warcraft.Model/Icon.cs
@@ -0,0 +1,39 @@
+﻿namespace Warcraft.Model
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Drawing;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Simple class that encapsulates a hitbox and a string icon type.
+    /// </summary>
+    public class Icon
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Icon"/> class.
+        /// </summary>
+        /// <param name="type">Icon type.</param>
+        /// <param name="x">Hitbox x pos.</param>
+        /// <param name="y">Hitbox y pos.</param>
+        /// <param name="w">Hitbox's width.</param>
+        /// <param name="h">Hitbox's height.</param>
+        public Icon(string type, int x, int y, int w, int h)
+        {
+            this.IconType = type;
+            this.Hitbox = new Rectangle(x, y, w, h);
+        }
+
+        /// <summary>
+        /// Icon's hitbox.
+        /// </summary>
+        public Rectangle Hitbox { get; set; }
+
+        /// <summary>
+        /// Icon's type.
+        /// </summary>
+        public string IconType { get; set; }
+    }
+}
