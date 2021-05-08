4df62122f9bfd3f7c24a0c31917c5aec40f2b0e1
neumann_norbert <neumann.norbert.norbert@gmail.com>
2021-05-08 11:07:37 +0200

    Added "some" supressions

--- OENIK_PROG4_2021_1_ZN8VJ5_GS6DL3/Warcraft.GameLogicTests/GlobalSuppressions.cs
+++ OENIK_PROG4_2021_1_ZN8VJ5_GS6DL3/Warcraft.GameLogicTests/GlobalSuppressions.cs
@@ -5,7 +5,5 @@
 
 using System.Diagnostics.CodeAnalysis;
 
-[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1633:File should have header", Justification = "<Pending>", Scope = "namespace", Target = "~N:Warcraft")]
 [assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1633:File should have header", Justification = "<Pending>")]
 [assembly: SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1404:Code analysis suppression should have justification", Justification = "<Pending>")]
-[assembly: SuppressMessage("Design", "CA1062:Validate arguments of public methods", Justification = "<Pending>", Scope = "member", Target = "~M:Warcraft.GameControl.TestController.OnRender(System.Windows.Media.DrawingContext)")]
